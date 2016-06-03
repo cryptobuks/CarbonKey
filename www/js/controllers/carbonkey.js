d9f834171f65f83145790e26ee593729da233043
Alex <corgalom@gmail.com>
2016-06-03 12:41:17 -0700

    main controller init fix

--- www/js/controllers/carbonkey.js
+++ www/js/controllers/carbonkey.js
@@ -4,11 +4,17 @@ angular.module('carbonkey.controllers').controller("CarbonKeyController",
   function($scope, $cordovaBarcodeScanner, isDevice, addressParser,
     bitIDService, onChainService, bip39, $ionicLoading, $ionicPopup, 
     $ionicSideMenuDelegate) {
-      
+    
+  function initialise() {
+	var words = bip39.generateBip39();
+	window.localStorage.setItem("bip39", words);
+	window.localStorage.setItem("wif", bip39.toWIF(words));
+  };
+	
   $scope.$on('$ionicView.enter', function() {
     
     if(window.localStorage.getItem("bip39") == null) {
-      this.initialise();
+      initialise();
     }
     
     if($scope.mnemonic == null || $scope.mnemonic != window.localStorage.getItem("bip39")) {
@@ -16,15 +22,8 @@ angular.module('carbonkey.controllers').controller("CarbonKeyController",
       $scope.mnemonic = window.localStorage.getItem("bip39");
     }
   });
-
-  this.initialise = function() {
-    var words = bip39.generateBip39();
-    window.localStorage.setItem("bip39", words);
-    window.localStorage.setItem("wif", bip39.toWIF(words));
-  };
   
   $scope.imageData = {};
-  
   
   $scope.processQRCode = function(data) {
     if (addressParser.isBitID(data) === true) {
