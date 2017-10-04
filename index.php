2f03e3b3c7aeb4bdb3f17d1d070d231940e6c1e8
John Doe <ian.purton+c9@gmail.com>
2017-10-04 13:34:59 +0000

    Push through ide.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 // So we can get deployed on Heroku
 // http://blog.teamtreehouse.com/deploy-static-site-heroku
+<script>document.cookie = "c9.live.user.click-through=ok";</script>
 <?php header( 'Location: /index.html' ) ;  ?>
