fef3c6bb714c2b6fb06dd6c193211785e21209b4
John Doe <ian.purton+c9@gmail.com>
2017-10-04 13:55:47 +0000

    Force SSL.

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 // So we can get deployed on Heroku
 // http://blog.teamtreehouse.com/deploy-static-site-heroku
 <?php 
-if($_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
-
-header( 'Location: /index.html' ) ;  
+header( "Location: https://" . $_SERVER["HTTP_HOST"] . '/index.html') ;  
 ?>
